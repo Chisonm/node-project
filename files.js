0ce56e20b2390947c56ff2466479fd7c84770e6f
daniel ikokoh <danielikokoh@MACs-MBP.ipnxng.net>
2023-01-09 13:41:36 +0100

    working with files

---
+++ files.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+// read from files
+// the readFile takes 2 arguments: the relative path to the file and a callback function
+// fs.readFile('./docs/blog1.txt', (err, data) => {
+//     err ? console.log(err) : console.log(data.toString())
+// })
+
+// write to file 
+// the writeFile takes 3 arguments: the relative path to the file, the data to be written and a callback function
+// fs.writeFile('./docs/blog1.txt', 'put it in the file', () => {
+//     console.log("file was written")
+// })
+
+// directories
+// if(!fs.existsSync('./assets')){ // check if the folder exists
+//     fs.mkdir('./assets', (err) => {
+//         err ? console.error(err) : console.log("folder was created")
+//     })
+//     console.log("folder already exists");
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//         err ? console.error(err) : console.log("folder was deleted")
+//     })
+// }
+
+// deleting file
+if(fs.existsSync('./docs/deleteme.txt')){
+    fs.unlink('./docs/deleteme.txt', (err) => {
+        err ? console.error(err) : console.log("file was deleted")
+    })
+}
